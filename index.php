2f0f4249a922070746afa8216d5a9a59733b7f2c
MrChuckNovice <teedji77@hotmail.fr>
2021-03-15 09:28:26 +0100

    pour JC

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,23 +8,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/main.css" rel="stylesheet">
     <title>Jeux d'Arcade</title>
-    <script src="js/search.js" defer ></script>
+    
 </head>
 <body>
+<header>
    <h1>The ARCADE</h1>
 <form>
     <div>
         <label for="search">le titre du jeu :</label>
         <input type="search" list="titre" autocomplete="off" name="search" id="search">
         <datalist id="titre"></datalist>
-         <button>Cliquez !</button>   
+         <button id ="btn">Cliquez !</button>   
     </div>
     
 </form>
-<div class="description" id="description"></div>
+</header>
+
+<div class="wrapper">
 <div  class="photo1"  id="photo1"></div>
 <div  class="photo2"  id="photo2"></div>
-<div  class="video"  id="video"></div>
-
+<div  class="video"  id="video">
+    <video controls width="250">
+        
+    </video>
+</div>
+<div class="description" id="description"></div>
+</div>
+<script src="js/search.js" defer ></script>
 </body>
 </html>
--- js/search.js
+++ js/search.js
@@ -2,6 +2,7 @@ const datalist = document.getElementById('titre');
 const descript = document.getElementById('description');
 const dinput = document.getElementById('search');
 const form = document.querySelector("form");
+const btn = document.getElementById('btn');
 
 dinput.addEventListener("input", (e) => {
 
@@ -32,10 +33,44 @@ dinput.addEventListener("input", (e) => {
 
             datalist.appendChild(option);
         });
+            if(search.value == option.innerText){
+                 
+                datalist.innerHTML = "";
+               // bug EXPloite wtf c'est comme un  addEventListener mais pas vraiment 
+                btn.click(); 
+            }
     });
    }
     
 });
 
-//faire une function qui s'affiche
+//faire une function qui valide le submit
 
+form.addEventListener("submit", (e) => { 
+  
+  e.preventDefault();
+  
+  const formData = new FormData(form);
+
+    fetch("inc/ajax.php",{
+
+        method: "POST",
+        body: formData,
+    }
+    )
+    .then(res => res.json())
+    .then(datas => {
+
+        descript.innerHTML = "";
+
+        datas.forEach((data) => {
+
+           descript.innerHTML += `<p> le jeu se nomme ${data.titre} développer par ${data.editeur}  <p>`;
+            
+         }) 
+ 
+    })
+
+//remets le formulaire à 0
+form.reset();
+});
